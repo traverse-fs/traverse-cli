a33ca3063cb2bba25a8aa11aeada845ae70b6bfd
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-09-22 20:11:46 -0700

    [COMMIT] First commit for traverse-cli. Adding minor changes version of traverse-fs

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * Package: 
+ * Author: Ganesh B
+ * Description: Nodejs npm module to traverse folder using code or cli or use glob patterns
+ * Install: npm i traverse-cli, npm i traverse-fs, npm i fssys
+ * Github: https://github.com/traverse-fs/traverse-cli
+ * npmjs Link: 
+ * File: index.js
+ * File Description: Get CLI Arguments function
+ * 
+*/
+
+require("cli.js");
--- src/config.js
+++ src/config.js
@@ -1,3 +1,16 @@
+/**
+ * 
+ * Package: 
+ * Author: Ganesh B
+ * Description: Nodejs npm module to traverse folder using code or cli or use glob patterns
+ * Install: npm i traverse-cli, npm i traverse-fs, npm i fssys
+ * Github: https://github.com/traverse-fs/traverse-cli
+ * npmjs Link: 
+ * File: config.js
+ * File Description: Get CLI Arguments function
+ * 
+*/
+
 module.exports = {
     "command": "",
     "usage": `\nUsage: \n <shellcommand> <command> [positional] \n\n Use to get detailed help for commands and positionals: \n <shellcommand> <command> -h `,
